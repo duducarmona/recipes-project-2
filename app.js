a6eaa759bfc08b943ec6cf681c36573172c89a65
Michael Mueller <michael@miniguide.co>
2020-03-12 19:50:31 +0100

    add dotenv package

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const usersRouter = require('./routes/users');
 
 const app = express();
 
+require('dotenv').config();
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
