eff0b04317e2498dd10e4c82b4c36e149c2d13ef
Michael Mueller <michael@miniguide.co>
2020-03-12 19:55:06 +0100

    place dotenv in variable

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
+const dotenv = require('dotenv');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
 
-require('dotenv').config();
+dotenv.config();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
