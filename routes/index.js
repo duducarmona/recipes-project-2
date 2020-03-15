f47db99e679e5dbbf755b57fbad342399b314c31
Michael Mueller <michael@miniguide.co>
2020-03-15 20:49:39 +0100

    add styling to buttons

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,50 @@ const express = require('express');
 
 const router = express.Router();
 
-/* GET home page. */
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+
+const bcryptSalt = 10;
+
 router.get('/', (req, res, next) => {
-  res.render('index', { layout: 'layout.hbs', title: 'Better Chef', loggedin: false });
+  res.render('index', {
+    title: 'Better Chef',
+    loggedin: false,
+  });
+});
+
+router.get('/register', (req, res, next) => {
+  res.render('register', {
+    title: 'Better Chef',
+    loggedin: false,
+  });
+});
+
+router.post('/register', (req, res, next) => {
+  const { username, password } = req.body;
+
+  User.findOne({ username })
+    .then((user) => {
+      console.log('Response is ', JSON.stringify(user));
+      if (user) {
+        res.render('index', { error: `Username ${user.username} already exists.` });
+      } else {
+        const salt = bcrypt.genSaltSync(bcryptSalt);
+        const hashPass = bcrypt.hashSync(password, salt);
+        console.log('Creating user', username);
+        User.create({
+          username,
+          password: hashPass,
+        })
+          .then(() => {
+            res.redirect('/recipes');
+          })
+          .catch((error) => next(error));
+      }
+    })
+    .catch((error) => {
+      next(error);
+    });
 });
 
 module.exports = router;
