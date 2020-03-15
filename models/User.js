0223eddfa1ac3d66e2bb2913951890309226f79a
Michael Mueller <michael@miniguide.co>
2020-03-15 18:25:20 +0100

    add models

---
+++ models/User.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    hashedPassword: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: {
+      createdAt: 'created_at',
+      updatedAt: 'updated_at',
+    },
+  },
+);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
