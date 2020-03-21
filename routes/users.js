f6de5a4f3d12bf0ea4fc05429e19996b887e4edb
Edu Carmona <dudu.carmona@gmail.com>
2020-03-21 12:39:11 +0100

    start user view

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,27 @@ const express = require('express');
 
 const router = express.Router();
 
-/* GET users listing. */
-router.get('/', (req, res) => {
-  res.send('respond with a resource');
+// /* GET users listing. */
+// router.get('/', (req, res) => {
+//   res.send('respond with a resource');
+// });
+
+// GET /recipes/:id
+router.get('/:id/update', (req, res, next) => {
+  const { id } = req.params;
+  Ingredient.find()
+    .then((ingredients) => {
+      Recipe.findById(id)
+        .populate('ingredients.ingredient')
+        .then((recipe) => {
+          res.render('update', {
+            recipe,
+            ingredients,
+          });
+        })
+        .catch(next);
+    })
+    .catch(next);
 });
 
 module.exports = router;
